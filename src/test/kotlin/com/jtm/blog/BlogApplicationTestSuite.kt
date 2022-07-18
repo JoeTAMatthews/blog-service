9a6a0ad0b8418675ee580c7c129b41f1964567f1
Joe Matthews <joetymatthews@gmail.com>
2022-07-18 18:43:09 +0100

    new: add test suite

--- src/test/kotlin/com/jtm/blog/BlogApplicationTestSuite.kt
+++ src/test/kotlin/com/jtm/blog/BlogApplicationTestSuite.kt
@@ -1,9 +1,14 @@
 package com.jtm.blog
 
+import com.jtm.blog.data.service.DraftServiceTest
+import com.jtm.blog.data.service.PostServiceTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 import org.junit.runners.Suite.SuiteClasses
 
 @RunWith(Suite::class)
-@SuiteClasses(value = [])
+@SuiteClasses(value = [
+    DraftServiceTest::class,
+    PostServiceTest::class,
+])
 class BlogApplicationTestSuite
