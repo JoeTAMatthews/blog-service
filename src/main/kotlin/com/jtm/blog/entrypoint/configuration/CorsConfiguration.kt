a14435baf99f0c03e86ead371c27916361d8b14d
Joe Matthews <joetymatthews@gmail.com>
2022-08-02 21:34:19 +0100

    refactor: add CORS configuration if not kubernetes profile.
    - The CORS configuration is needed for testing outside the kubernetes cluster.

--- src/main/kotlin/com/jtm/blog/entrypoint/configuration/CorsConfiguration.kt
+++ src/main/kotlin/com/jtm/blog/entrypoint/configuration/CorsConfiguration.kt
@@ -1,12 +1,14 @@
 package com.jtm.blog.entrypoint.configuration
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.web.reactive.config.CorsRegistry
 import org.springframework.web.reactive.config.EnableWebFlux
 import org.springframework.web.reactive.config.WebFluxConfigurer
 
 @EnableWebFlux
 @Configuration
+@Profile("!kubernetes")
 open class CorsConfiguration: WebFluxConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
