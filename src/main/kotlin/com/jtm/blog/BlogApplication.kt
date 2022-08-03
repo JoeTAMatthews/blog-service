754da16ade44ce85e40aa2e260487a6e30fe6608
Joe Matthews <joetymatthews@gmail.com>
2022-08-03 17:13:35 +0100

    new: add kubernetes discovery client.

--- src/main/kotlin/com/jtm/blog/BlogApplication.kt
+++ src/main/kotlin/com/jtm/blog/BlogApplication.kt
@@ -2,7 +2,9 @@ package com.jtm.blog
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
+@EnableDiscoveryClient
 @SpringBootApplication
 open class BlogApplication
 
